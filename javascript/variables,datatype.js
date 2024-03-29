cb9fb6a6251464438658353f85456549aed41dd2
coderun13 <snehalsingh.0407@gmail.com>
2024-03-29 12:35:47 +0530

    datatype and variables

---
+++ javascript/variables,datatype.js
@@ -0,0 +1,24 @@
+
+console.log("Lets start datatypes and variables");
+
+var a = 5;
+// a = a + 1
+let b = 6;
+let c = "snehal";
+let _a = "aryan";
+
+// console.log(a +  b + 8);
+// console.log(typeof a, typeof b, typeof c);
+{
+    let a = 66;
+    console.log(a)
+}
+
+
+let x = "Javascript";
+let y = 10;
+let z = 3.55;
+const p = true;
+let q = undefined;
+let r = null;
+
