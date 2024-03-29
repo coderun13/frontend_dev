e93ed73f4a3c55670fcdbc3b94a6a3e5a79fea02
coderun13 <snehalsingh.0407@gmail.com>
2024-03-29 12:36:22 +0530

    datatypes and variables

--- javascript/variables,datatype.js
+++ javascript/variables,datatype.js
@@ -22,3 +22,18 @@ const p = true;
 let q = undefined;
 let r = null;
 
+console.log(x, y, z, p, q, r)
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r)
+
+
+let o = {
+    "name": "Snehal",
+    "role": coder,
+    "is_beginer": true
+}
+
+console.log(o);
+o.salary = "1crores";
+console.log(o);
+o.salary = "50lakhs";
+console.log(o);
