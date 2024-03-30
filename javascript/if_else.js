5f763fbfd3764d755b651588c81ac01cbc5f2ab6
coderun13 <snehalsingh.0407@gmail.com>
2024-03-30 19:01:28 +0530

    conditional statements

---
+++ javascript/if_else.js
@@ -0,0 +1,39 @@
+/**
+  if-else statement
+  */
+
+console.log("message before if statement");
+
+let age = 27;
+
+if (age >= 18){
+  console.log("You can vote");
+}
+
+if (age <= 18){
+    console.log("You cannot vote");
+}
+
+console.log("message after if statement");
+
+if (age >= 20){
+    console.log("You are in your 20s");
+}
+
+let firstName = "snehal";
+if(firstName == "snehal"){
+    console.log(`Welcome ${firstName}`)
+}
+
+
+/**
+ * Traffic light system practice
+ */
+
+
+
+
+
+
+
+
