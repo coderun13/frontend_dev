f3ca146e122ee15a7f63b182c333b42d6374d3ec
coderun13 <snehalsingh.0407@gmail.com>
2024-03-31 01:30:04 +0530

    strings

---
+++ javascript/strings.js
@@ -0,0 +1,37 @@
+
+
+/**
+ * string- immutable(no changes can be made)
+ */
+
+/**
+ * methods- actions that can be performed on objects.
+ */
+
+/*trim method(trim extra spaces)*/
+
+let msg = "   hello  ";
+msg.trim()
+
+
+/**
+ * toUpperCase() and toLowerCase()
+ */
+
+let str = "random string";
+str.toUpperCase();
+str.toLowerCase();
+
+
+/**
+ * string methods with arguments
+ */
+
+stringName.method(arg)/* format */
+
+let n= "Newtocoding";
+n.indexOf("coding") 
+n.indexOf("w")
+n.indexOf("o")
+n.indexOf("l")
+
