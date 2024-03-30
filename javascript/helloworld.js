6cdb87c5099bfe51149af39bc59ff01a9951e2e7
coderun13 <snehalsingh.0407@gmail.com>
2024-03-31 01:14:21 +0530

    alerts and prompts

--- javascript/helloworld.js
+++ javascript/helloworld.js
@@ -9,3 +9,33 @@ console.log(1234);
 
 console.log(2+2);
 
+
+/**
+ * alert(pop-up msg is appears)
+ */
+
+/*alert{"something is wrong"};
+
+
+/**
+ * error message(appears in red color)
+ */
+
+/*console.error{"this is an error msg"};
+
+
+/**
+ * warning message(appears in yellow color)
+ */
+
+/*console.warn{"this is a warning msg"};*/
+
+
+/**
+ * prompts(dailogue box appears)
+ */
+
+/*prompt{"enter your name : "};
+
+let firstName = prompt{"enter your name : "};
+console.log(firstName);*/ 
