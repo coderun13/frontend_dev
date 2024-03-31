76e72c72b863aa4b7b6dde96c7636ef8dda6ee25
coderun13 <snehalsingh.0407@gmail.com>
2024-03-31 16:39:23 +0530

    arrays

---
+++ javascript/arrays.js
@@ -0,0 +1,49 @@
+
+/**
+ * array - linear collection of things 
+ * arrays are mutable (values can be changes)
+ */
+
+let students = ["snehal", "aryan", "aman", "rohan"];
+console.log(students);
+
+
+let marks = [20,30,89,97,100,42];
+console.log(marks);
+
+
+let info = ["snehal",18,"js"];
+console.log(students);
+
+console.log(info.length); //array length//
+
+console.log(info[0]); //string at index 0//
+
+console.log(info[0][0]);//string's 0 index character//
+
+console.log(info[0].length);//0 index string's length//
+
+let emparr = []; //empty array//
+console.log(emparr.length);
+
+
+let fruits = ["mango","apple","litchi"];
+
+fruits[0] = "grapes"; //mango is changed to grapes//
+
+console.log(fruits);
+
+console.log(fruits.length);
+
+fruits[10] = "papaya"; //elements can be added to any index//
+
+console.log(fruits.length);
+
+
+/**
+ * Array methods(push,pop,shift,unshift)
+ */
+
+
+
+
