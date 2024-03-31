21133b720bf91516f421f457e35a0d1a53be7813
coderun13 <snehalsingh.0407@gmail.com>
2024-03-31 15:49:23 +0530

    string methods

--- javascript/strings.js
+++ javascript/strings.js
@@ -30,8 +30,21 @@ str.toLowerCase();
 stringName.method(arg)/* format */
 
 let n= "Newtocoding";
-n.indexOf("coding") 
+
+n.indexOf("coding") /*index- first occurance of letter in string*/
 n.indexOf("w")
 n.indexOf("o")
 n.indexOf("l")
 
+
+/*slice*/
+/* str.slice(start,end) */
+
+n.slice(5) /* "coding" */
+n.slice(1,4) /* "ewt"(4 not included) */ 
+n.slice(-1)// 11-1=>10(ans is g)//
+ 
+/*replace and repeat */
+
+n.replace("to","to do") //new to do coding 
+n.repeat(2)//newtocodingnewtocoding
