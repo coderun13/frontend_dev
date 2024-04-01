6ba672279ffb9eb8fbe9c70cc90ac14a902e4909
coderun13 <snehalsingh.0407@gmail.com>
2024-04-01 23:08:56 +0530

    loops with arrays

--- javascript/loops.js
+++ javascript/loops.js
@@ -80,11 +80,41 @@ if(guess == favMovie) {
     console.log("you quit")
  }*/
 
+// break keyword
+
+let j=1;
+
+while(j<=5) {
+    if(j == 3) {
+        break;
+    }
+    console.log(j);
+    j++;
+}
+console.log("we used break at 3");
+
+//loops with arrays
+
+let fruits = ["mango","apple","orange","litchi","banana"]
+fruits.push("pineapple");   
+for(let i=0; i<fruits.length; i++){
+    console.log(i,fruits[i]);
+}
+
+//nested loops with nested arrays
+
+let heroes = [
+    ["ironman","spiderman","thor"],
+    ["superman","wonder women","flash"]
+]
+
+for(let i=0; i<heroes.length; i++){
+    console.log(`list #${i}`);
+    for(let j=0; j<heroes[i].length; j++){
+            console.log(heroes[i][j]);
+        }
+    }
 
 
 
-
-
-
-
-
+    
