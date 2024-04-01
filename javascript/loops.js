4832324186a594a7cab96d7a1790ccc8104cb42e
coderun13 <snehalsingh.0407@gmail.com>
2024-04-01 19:45:48 +0530

    for loop

---
+++ javascript/loops.js
@@ -0,0 +1,42 @@
+
+// loops- used to iterate a piece of code
+
+/**
+ * for loop
+ */
+// for(initialisation;condition;updation){
+// do something
+//}
+
+console.log("nos. from 1-5");
+for(let i=1; i<=5; i++){
+console.log(i);
+}
+
+console.log("nos. from 5-1");
+for(let i=5; i>=1; i--){
+    console.log(i);
+    }
+
+console.log("odd nos. from 1-15");    
+    for(let i=1; i<=15; i+=2){
+        console.log(i);
+        }    
+
+
+ console.log("even nos. from 2-10");    
+    for(let i=2; i<=10; i+=2){
+        console.log(i);
+        }   
+
+ console.log("table of 5");    
+    for(let i=5; i<=50; i+=5){
+        console.log(i);
+        }  
+
+
+
+
+
+
+
