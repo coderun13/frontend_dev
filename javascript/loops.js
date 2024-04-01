9b6763e09639fce1833a1a9e478ca020a8c67879
coderun13 <snehalsingh.0407@gmail.com>
2024-04-01 21:13:43 +0530

    while loop

--- javascript/loops.js
+++ javascript/loops.js
@@ -4,6 +4,7 @@
 /**
  * for loop
  */
+
 // for(initialisation;condition;updation){
 // do something
 //}
@@ -33,6 +34,53 @@ console.log("odd nos. from 1-15");
     for(let i=5; i<=50; i+=5){
         console.log(i);
         }  
+ 
+/**
+ * Nested for loop
+ */
+
+for(let i=1; i<=2; i++){
+    console.log(`outer loop ${i}`);
+    for(let j=1; j<=3; j++){
+        console.log(j);
+    }
+}
+
+
+/**
+ * While loop
+ */
+
+//while(condition){
+//do something
+//}
+
+console.log("num from 1-5");
+let i = 1;
+while(i<=5){
+    console.log(i);
+ i++;
+}
+
+//favourite movie guessing game
+
+/*const favMovie = "avatar";
+
+let guess = prompt("guess my favorite movie");
+
+while(guess != favMovie && (guess != "quit")){
+    console.log("wrong guess");
+    guess = prompt("guess my favorite movie");
+}
+
+if(guess == favMovie) {
+    console.lof("congrats!!");
+
+} else {
+    console.log("you quit")
+ }*/
+
+
 
 
 
