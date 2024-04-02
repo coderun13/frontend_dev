2c9ed7fb099ea57491606ec10c3f4b2c1c5a60b7
coderun13 <snehalsingh.0407@gmail.com>
2024-04-02 14:33:39 +0530

    objects

--- javascript/object.js
+++ javascript/object.js
@@ -4,6 +4,7 @@
  * objects are a collection of properties
  */
 
+
 // creating objects(object doesn't have any specific order)
 
 let student = {
@@ -15,6 +16,7 @@ let student = {
 
 console.log(student);
 
+
 // creating a twitter post
 
 const post = {
@@ -25,16 +27,19 @@ const post = {
 };
 console.log(post);
 
+
 //get values
 
 console.log(post.username);
 console.log(post.likes);
+
 
 //add_update values 
 
 student.city = "mumbai";  //value update from delhi to mumbai
 student.gender = "female"; // new value added
 console.log(student);
+
 
 //nested objects
 //object of objects
@@ -50,3 +55,45 @@ const classInfo = {
         city: "bokaro"
     },
 };
+console.log(classInfo.snehal);
+console.log(classInfo.aryan.grade);
+
+
+//Array of Objects
+
+const classinfo = [
+    {
+        name: "snehal", 
+        grade: "A+",
+        city: "delhi"
+    },
+    {
+        name: "aryan",
+        grade: "A",
+        city: "bokaro"
+    },
+];
+console.log(classinfo);
+console.log(classinfo[1]);
+console.log(classinfo[1].name);
+
+
+//math object
+
+console.log(Math.PI);
+console.log(Math.E);
+console.log(Math.abs(-12.5));
+console.log(Math.pow(2,4)); //a**b
+console.log(Math.floor(5.6)); //round off(nearest smallest number)
+console.log(Math.ceil(5.6)); //round off(nearest largest number)
+console.log(Math.random()); // any number range from 0 to 1(1 excluded)
+
+
+//random integers
+//(from 1 to 10)
+
+let num = Math.random(); // step 1
+num = num*10; // step 2
+num = Math.floor(num); // step 3
+num = num + 1; // step 4
+console.log(num); //print
