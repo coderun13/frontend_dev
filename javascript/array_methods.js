78b051d918b4e2acfa7b0ea4a4ea4270b3d4e124
coderun13 <snehalsingh.0407@gmail.com>
2024-04-04 21:41:21 +0530

    map and filter function

--- javascript/array_methods.js
+++ javascript/array_methods.js
@@ -11,34 +11,41 @@ arr.forEach(print);
 
 
 
-//
+//Map(make new array to store the values)
 
+let num = [2,3,4,5];
+let double = num.map(function(el){
+    return el*2;
+});
+console.log(double);
 
+let students = [
+    {
+        name: "snehal",
+        marks: 95,
+    },
+    {
+        name: "aryan",
+        marks: "92",
+    },
+];
 
+let gpa = students.map((el) => {
+    return el.marks / 10;
+});
+console.log(gpa);
 
+//filter function
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+let nums = [1,2,3,4,5,6,7,8,9,10,12,11];
+let ans = nums.filter((el) => {
+    return el%2 == 0; //even -> true
+});
+let an = nums.filter((el) => {
+    return el%2 != 0; // odd -> true
+});
+console.log(ans);
+console.log(an);
 
 
 
